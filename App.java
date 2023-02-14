dc678874ce1a5d2c8893d7fee9d9f07ccf156451
Eoin Chedzey <eoinisnow@gmail.com>
2023-02-14 23:32:17 +0000

    refactored code based on Fowler’s Refactoring text

--- App.java
+++ App.java
@@ -4,13 +4,19 @@ import MovieRentalSystem.Rental;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
         Customer customer = new Customer("John");
-        System.out.println(customer.statement());
+        System.out.println("Added new Customer: " + customer.getName() );
+        System.out.println("Initial Customer Statement:  \n" + customer.statement() + "\n");
 
         Movie movie = new Movie("The Godfather", 1);
         Rental rental = new Rental(movie, 1);
         customer.addRental(rental);
-        System.out.println(customer.statement());
+
+        Movie movie2 = new Movie("The Godfather Part II", 0);
+        Rental rental2 = new Rental(movie2, 3);
+        customer.addRental(rental2);
+
+        System.out.println("Update Customer Statement:  \n" + customer.statement());
     }
 }
